9495db78939ed0249caaa527386c4252dbf3d083
LudvigHN <ludvighaukrognielsen@gmail.com>
2026-09-03 11:03:43 +0200

    copied and extended first object

--- data-som-objekter/script.js
+++ data-som-objekter/script.js
@@ -1,6 +1,20 @@
+// const person = {
+//     navn: "John John",
+//     alder: 98,
+//     by:"roskilde",
+//     arbejde:"landmand"
+// }
+
 const person = {
-    navn: "John John",
-    alder: 98,
-    by:"roskilde",
-    arbejde:"landmand"
+    name: "John John",
+    age: 98,
+    city:"Roskilde",
+    job:"farmer",
+    male:true,
+    pet:{
+        type:"cat",
+        name:"Jeff",
+        age:12,
+    },
+    hobbies:["football","cycling","swimming"],
 }
