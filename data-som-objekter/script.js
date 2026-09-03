35dd6d56adbe5a8ea38db239e1d272c54a043e3c
LudvigHN <ludvighaukrognielsen@gmail.com>
2026-09-03 12:07:11 +0200

    make foreach loop add class to the items

--- data-som-objekter/script.js
+++ data-som-objekter/script.js
@@ -20,16 +20,17 @@ const person = {
 }
 
 const students = [
-    {name:"Sofie",age:25,course:"Webudvikler"},
-    {name:"Lukas",age:20,course:"Mediegrafiker"},
-    {name:"Emil",age:28,course:"Teknisk Designer"},
+    { name: "Sofie", age: 25, course: "Webudvikler" },
+    { name: "Lukas", age: 20, course: "Mediegrafiker" },
+    { name: "Emil", age: 28, course: "Teknisk Designer" },
 ]
 
 const insertDOM = document.getElementById("insert")
 
-students.forEach(function(student){
+students.forEach(function (student) {
     const message = `name: ${student.name}, Alder: ${student.age}, Uddannelse: ${student.course}`
+    divElement.classList.add("student")
     const divElement = document.createElement("div")
-    divElement.textContent= message
+    divElement.textContent = message
     insertDOM.append(divElement)
 })
